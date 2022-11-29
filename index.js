27e324114f72d814021fdb56a47d1cbacfb196c1
Loise <loise.wambui@student.moringaschool.com>
2022-11-29 07:56:26 +0300

    Completed arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-
 // Write your code here
+//multiplication
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+console.log(multiply);
+
+//randomInt
+let random = Math.floor(Math.random()) + 1;
+console.log(random);
+
+//mod
+let num3 = 24;
+let num4 = 5;
+let mod = num3 % num4;
+console.log(mod);
+
+//max
+let max = Math.max(8, 3, 19, 20);
+console.log(max);
